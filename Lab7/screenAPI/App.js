284ede8c77921b2c9d377a4f239da87b1b588474
TienSinh03 <zatos1232@gmail.com>
2024-10-08 17:04:53 +0700

    stable GUI

--- Lab7/screenAPI/App.js
+++ Lab7/screenAPI/App.js
@@ -5,6 +5,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import ScreenAPI_01 from './component/ScreenAPI_01';
 import ScreenAPI_02 from './component/ScreenAPI_02';
+import ScreenAPI_03 from './component/ScreenAPI_03';
 
 
 const Stack = createNativeStackNavigator();
@@ -14,8 +15,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="ScreenAPI_01" component={ScreenAPI_02} options = {{headerShown:false}} />
+        <Stack.Screen name="ScreenAPI_01" component={ScreenAPI_01} options = {{headerShown:false}} />
         <Stack.Screen name="ScreenAPI_02" component={ScreenAPI_02}  options = {{headerShown:false}} />
+        <Stack.Screen name="ScreenAPI_03" component={ScreenAPI_03}  options = {{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Lab7/screenAPI/component/ScreenAPI_02.js
+++ Lab7/screenAPI/component/ScreenAPI_02.js
@@ -3,40 +3,55 @@ import { StyleSheet, Text, View, SafeAreaView,Image, TextInput,FlatList, Touchab
 import React,{useEffect, useState} from 'react';
 
 
-const data = [
-    {
-        'id': 1, 
-        'name': 'Twinkle', 
-        'image': '../assets/avatar.png', 
-        'job': [
-            'To check email', 
-            'UI task web page', 
-            'Learn javascript basic',
-            'Learn HTML advanced',
-            'Medical App UI',
-            'Learn Java'
-        ]
-    },
-]
 
-const Item = ({job}) => (
-    <View style={styles.searchItem}>        
-            <View style={{paddingHorizontal:10, paddingTop:10}}>
-                <Text style={styles.searchItemTitle}>{job}</Text>
-            </View>
-          
+const Task = ({jobName}) => (
+   <View style={[styles.task, styles.elevation]}>
+
+          <TouchableOpacity >
+              <Image source={require('../assets/tickl.png')} style = {{resizeMode:'cover', width: 27, height: 27, }}/>
+          </TouchableOpacity>
+
+          <View style={styles.textWrapper}>
+            <Text style={styles.text}>{jobName}</Text>
+          </View>
+          <TouchableOpacity >
+              <Image source={require('../assets/edit.png')} style = {{resizeMode:'cover', width: 27, height: 27, }}/>
+          </TouchableOpacity>
     </View>
-  );
+);
   
 
-const ScreenAPI_01 = ({navigation,route}) => {
+const ScreenAPI_02 = ({navigation,route}) => {
+
+    const [data, setData] = useState();
+    const [task, setTask] = useState({
+        content: '',
+    });
+
+    useEffect(() => {
+        fetchApi();
+    }, []);
+
+
+    const fetchApi = async () => {
+        try {
+          const respone = await fetch('https://67039ce3ab8a8f892730d9f4.mockapi.io/api/task');
+          const json = await respone.json();
+          setData(json);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+
     return (
         <SafeAreaView style={styles.container}>
            {/* View header */}
            <View style={styles.header}>
                 {/* View nav information  */}
                 <View style={styles.navInfo}>
+                  <TouchableOpacity onPress={() => navigation.goBack()}>
                     <Image source={require('../assets/muiten.png')} style = {{resizeMode:'cover', width: 22, height: 22, }}/>
+                  </TouchableOpacity>
 
                     <View style={{flexDirection:'row', alignItems:'center'}}>
                          <Image source={require('../assets/userimage.png')} style = {{resizeMode:'cover', marginRight:10}}/>
@@ -46,7 +61,7 @@ const ScreenAPI_01 = ({navigation,route}) => {
                          </View>
                     </View>
                 </View>
-                <View style={{flex:2}}>
+                <View style={{flex:3}}>
                     <View style={styles.inputEmail}>
                         <Image source={require('../assets/iconSearch.png')} style = {{resizeMode:'cover', width: 27, height: 27, marginHorizontal:10}}/>
                         <TextInput placeholder="Search" style={styles.textInput}/>
@@ -56,15 +71,31 @@ const ScreenAPI_01 = ({navigation,route}) => {
 
            {/* View list */}
            <View style={styles.list}>
-                {/* display list */}
-                <View style={{ flex: 1 }}>
-                <FlatList
-                    data={data}
-                    renderItem={({item}) => <Item job={item.job} />}
-                    keyExtractor={item => item.id}
-                    scrollEnabled={true}
-                />
-                </View>
+            
+            <View style={{flex:6}}>
+              <FlatList
+                      data={data}
+                      renderItem={({ item }) => (
+                          <Task
+                            key={item.id}
+                            jobName={item.content}
+                            // onDelete={() => deleteTask(item.id)}
+                          />
+                        )}
+                      keyExtractor={item => item.id}
+                      scrollEnabled={true}
+                  />
+            </View>
+                
+                 <View style={{flex:2,justifyContent:'flex-start'}}>
+                  <TouchableOpacity style={styles.buttonAdd}
+                    onPress={() => navigation.navigate('ScreenAPI_03')}
+                  
+                  >
+                      <Image source={require('../assets/iconPlus.png')} style = {{resizeMode:'cover', width: 27, height: 27, }}/>
+                  </TouchableOpacity>
+                </View>       
+                         
            </View>
         </SafeAreaView>
     );
@@ -85,6 +116,7 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between',
         margin: 20,
         alignItems:'center',
+        width:'90%'
     },
     inputEmail:{
         flexDirection: 'row',
@@ -92,13 +124,41 @@ const styles = StyleSheet.create({
         borderWidth:1,
         paddingVertical:10,
     
-        width:334,
+        width:'90%',
         borderRadius:10,
+        marginLeft:20,
     },
     list: {
         flex:7,
-    }
-
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    task: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        backgroundColor: "rgba(222, 225, 230, 0.47)",
+        padding: 15,
+        borderRadius: 24,
+        margin: 10,
+        width: '85%',
+        marginLeft:30
+     },
+     textWrapper: {
+        // flex: 1,
+     },
+     text: {
+        fontSize: 16,
+        fontWeight:'bold',
+     },
+     delete: {
+        color: "red",
+     },
+     elevation: {
+      elevation: 20,
+      shadowColor: 'black',
+    },
+    buttonAdd:{backgroundColor:'#00BDD6', marginTop:25,width: 50, height: 50, borderRadius: 25, justifyContent:'center', alignItems:'center'}
 });
 
-export default ScreenAPI_01;
+export default ScreenAPI_02;
